ad823c5733ea298f98f4ac9bf3220d772b276a80
cshoaib05 <choudharyshoaib05@gmail.com>
2019-11-30 23:17:47 +0530

    Api integrated

--- ANDROID/app/src/main/java/com/wancstudios/fireextinguisher/DailyRecords.java
+++ ANDROID/app/src/main/java/com/wancstudios/fireextinguisher/DailyRecords.java
@@ -1,16 +1,75 @@
 package com.wancstudios.fireextinguisher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 public class DailyRecords extends AppCompatActivity {
+
+    public String url = "http://192.168.0.112:8000/api/dailyData";
+    RecyclerView recyclelist;
+    public ArrayList<DailyRecordsContainer> Dailyrecordlist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recyclerview_dailyrecords);
 
+        recyclelist = findViewById(R.id.Dailyrecordrecycle);
+        Dailyrecordlist = new ArrayList<>();
+        RequestData();
+    }
 
+    public void RequestData() {
+        final RequestQueue requestQueue = Volley.newRequestQueue(DailyRecords.this);
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+//                            String name = response.getJSONObject(i).getString("name");
+//                            String othername = response.getJSONObject(i).getString("customer");
+//                            String quantity = response.getJSONObject(i).getString("quantity");
+//                            String price = response.getJSONObject(i).getString("price_sold");
+//                            String date = response.getJSONObject(i).getString("date");
+//
+//                            Dailyrecordlist.add(new DailyRecordsContainer(name,price,quantity,othername,date));
+                        }
+                        recyclelist.setLayoutManager(new LinearLayoutManager(DailyRecords.this));
+                        recyclelist.setAdapter(new DailyRecordsistAdapter(DailyRecords.this, Dailyrecordlist));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error", error.toString());
+                    requestQueue.stop();
+                }
+            }
+        ) {
+
+        };
+        requestQueue.add(jsonObjectRequest);
     }
 }
--- ANDROID/app/src/main/java/com/wancstudios/fireextinguisher/ItemListAdapter.java
+++ ANDROID/app/src/main/java/com/wancstudios/fireextinguisher/ItemListAdapter.java
@@ -1,28 +1,39 @@
 package com.wancstudios.fireextinguisher;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.bumptech.glide.Glide;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class ItemListAdapter extends RecyclerView.Adapter
+import java.util.ArrayList;
+
+
+public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemListViewHolder>
 {
     private Context context;
 
-    public ItemListAdapter(Context context)
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+    private ArrayList<ItemContainer> data;
+
+    public ItemListAdapter(Context context, ArrayList<ItemContainer> data)
     {
          this.context=context;
+         this.data = data;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    public ItemListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.container_item,parent,false);
@@ -30,14 +41,23 @@ public class ItemListAdapter extends RecyclerView.Adapter
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position)
+    public void onBindViewHolder(@NonNull final ItemListViewHolder holder, int position)
     {
-
+        holder.item_name.setText(data.get(position).item_name);
+        holder.item_amount.setText(data.get(position).item_amount);
+        holder.item_quantity.setText(data.get(position).item_quantity);
+        storageRef.child(data.get(position).item_name+".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri)
+            {
+                Glide.with(context).load(uri).into(holder.image);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
 
