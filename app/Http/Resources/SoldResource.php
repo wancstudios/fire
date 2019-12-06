7af1bbb20ddb85a77dfbd40dfc8e012cd77e3fad
Rizzu1023 <mrizwan9022@gmail.com>
2019-12-06 23:41:19 +0530

    id added to sold api

--- app/Http/Resources/SoldResource.php
+++ app/Http/Resources/SoldResource.php
@@ -16,6 +16,7 @@ class SoldResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'customer' => $this->customer,
             'quantity' => $this->quantity,
