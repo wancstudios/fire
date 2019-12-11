635de0f508d2ba534e72bab219689169357302d8
cshoaib05 <choudharyshoaib05@gmail.com>
2019-12-12 00:44:21 +0530

    SPlash screen aadded

---
+++ ANDROID/app/src/main/java/com/wancstudios/fireextinguisher/splashscreen.java
@@ -0,0 +1,26 @@
+package com.wancstudios.fireextinguisher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(splashscreen.this,MainActivity.class);
+                splashscreen.this.startActivity(mainIntent);
+                splashscreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
